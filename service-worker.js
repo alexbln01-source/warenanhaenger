347712501e01170b1ac51f6993c460d4a9cdf979
alexbln01-source <alexbln01@googlemail.com>
2026-02-09 19:37:04 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,15 @@
 // service-worker.js
-self.addEventListener("install", () => {
+
+self.addEventListener("install", event => {
   console.log("[SW] installiert");
 });
 
 self.addEventListener("activate", event => {
   console.log("[SW] aktiviert");
   event.waitUntil(self.clients.claim());
+});
+
+// 🔑 DAS IST DER SCHLÜSSEL
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
