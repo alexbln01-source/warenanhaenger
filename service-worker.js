4a801751dc81f195b78dd3443dafadc87b35e368
alexbln01-source <alexbln01@googlemail.com>
2026-02-09 18:32:49 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,11 @@
 // service-worker.js
 
-const CACHE_NAME = "warenanhaenger-v1";
-
-// Optional: später für Caching
 self.addEventListener("install", event => {
-  // NICHT skipWaiting → wichtig für PWA-Install
   console.log("[SW] Installiert");
+  self.skipWaiting(); // WICHTIG für Updates
 });
 
 self.addEventListener("activate", event => {
   console.log("[SW] Aktiviert");
   event.waitUntil(self.clients.claim());
 });
-
-/*
-  Update-Info:
-  Der Service Worker selbst meldet KEIN Update.
-  Das macht die Seite (JS) über reg.waiting
-*/
