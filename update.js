314daf25a67668ab389aa16e98f8608e41c1f691
alexbln01-source <alexbln01@googlemail.com>
2026-02-11 19:24:58 +0100

    Update update.js

--- update.js
+++ update.js
@@ -1,7 +1,33 @@
 (function () {
 
     fetch("version.json?nocache=" + Date.now(), {
-        cache: "no-store"
+        cache: "no-store"(function () {
+
+    const CHECK_URL = "version.json?v=" + Date.now();
+
+    fetch(CHECK_URL, { cache: "no-store" })
+        .then(res => res.json())
+        .then(data => {
+
+            const currentVersion = localStorage.getItem("app_version");
+
+            if (currentVersion !== data.version) {
+
+                localStorage.setItem("app_version", data.version);
+
+                // 🔥 kompletter Hard Reload der App
+                window.location.href =
+                    window.location.origin +
+                    window.location.pathname +
+                    "?update=" + Date.now();
+            }
+
+        })
+        .catch(() => {
+            console.log("Update Check fehlgeschlagen");
+        });
+
+})();
     })
     .then(response => response.json())
     .then(data => {
